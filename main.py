6cd46b2e54a5451801eed87beee9d2c9f1e95461
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-30 14:05:24 +0430

    saving data into google sheets.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 from datetime import datetime
 
+N_API_KEY = ""
+N_APP_ID = ""
 
-API_KEY = ""
-APP_ID = ""
+sheet_endpoint = "https://api.sheety.co/f7517ad33ca76c0452786a377a26331f/workoutTracking/sheet1"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 exercise_text = input("Tell me witch exercises you did today?: ")
@@ -17,7 +18,22 @@ params = {
     "query": exercise_text,
 }
 
-today = datetime.now()
-
 response = requests.post(url=exercise_endpoint, json=params, headers=exercise_headers)
-print(response.json())
+result = response.json()
+
+today = datetime.now().strftime("%Y%m%d")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_input = {
+        "workout": {
+            "date": today,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    sheet_response = requests.post(sheet_endpoint, json=sheet_input)
+    print(sheet_response)
+
